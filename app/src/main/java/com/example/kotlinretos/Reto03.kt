9c57d3483c1995d3739e20ec36f466281c3ffaa4
Jose Miguel Alejandro Sepulveda Pino <josealejandrosepulvedapino@gmail.com>
2022-11-26 23:00:46 -0300

    Reto03 terminado

---
+++ app/src/main/java/com/example/kotlinretos/Reto03.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinretos
+
+/*
+ * Enunciado: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+ * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
+ * 0, 1, 1, 2, 3, 5, 8, 13...
+ *
+ */
+
+fun main() {
+    var a :Long = 0 //Se definen como Long para que los ultimos numeros de la serie fibonacci no arrojen numeros erroneos
+    var b :Long = 1 //Se definen como Long para que los ultimos numeros de la serie fibonacci no arrojen numeros erroneos
+    var x = 0
+    while (x<49) {
+        var c = a + b
+        a = b
+        b = c
+        x++
+        println(c)
+    }
+}
+
