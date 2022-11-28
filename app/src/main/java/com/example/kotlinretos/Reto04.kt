fbb8a47f131de38f1e305d6e911f5916b446cc55
Jose Miguel Alejandro Sepulveda Pino <josealejandrosepulvedapino@gmail.com>
2022-11-27 22:56:01 -0300

    Reto04 terminado

--- app/src/main/java/com/example/kotlinretos/Reto04.kt
+++ app/src/main/java/com/example/kotlinretos/Reto04.kt
@@ -1,10 +1,21 @@
 package com.example.kotlinretos
-
 /*
-
+* Enunciado: Crea una función que reciba días, horas, minutos y segundos (como enteros) y retorne su resultado en milisegundos.
  */
 
-
 fun main() {
+    print(toMil(1,0,34,0))
+    print(" Milisegundos")
+}
+
+fun toMil(dias: Int, horas: Int, minutos: Int, segundos: Int) : Int {
+    val diasMil = dias * 86400000
+    val horasMil = horas * 3600000
+    val minutosMil= minutos * 60000
+    val segundosMil= segundos * 1000
+    val totalMil = diasMil + horasMil + minutosMil + segundosMil
+    return totalMil
 
 }
+
+
