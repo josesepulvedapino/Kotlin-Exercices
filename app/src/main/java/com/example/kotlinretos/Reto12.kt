2ba47c3007aba8ccc079e35d4d1b60ad90568180
Jose Miguel Alejandro Sepulveda Pino <josealejandrosepulvedapino@gmail.com>
2022-11-29 16:48:41 -0300

    Reto12 terminado v2

--- app/src/main/java/com/example/kotlinretos/Reto12.kt
+++ app/src/main/java/com/example/kotlinretos/Reto12.kt
@@ -1,24 +1,16 @@
 package com.example.kotlinretos
 
-fun main(args: Array<String>) {
-    println("Your fortune is: ${getFortune(5)}")
+fun main() {
+    println("Your fortune is: ${getFortune(getBirthday())}")
 }
 
-fun getBirthday(): Int? {
-    println("Enter your birthday: ")
+fun getBirthday(): Int {
+    print("Enter your birthday: ")
     val birthday = readLine()?.toIntOrNull() ?: 1
     return birthday
 }
 
 fun getFortune(birthday: Int) : String {
-    val fortunes = listOf(
-        "You will have a great day!",
-        "Things will go well for you today.",
-        "Enjoy a wonderful day of success.",
-        "Be humble and all will turn out well.n",
-        "Today is a good day for exercising restraint.",
-        "Take it easy and enjoy life!",
-        "Treasure your friends because they are your greatest fortune.")
     when (birthday) {
         in 2..5 -> return "You will have a great day!"
         in 6..12 -> return "Things will go well for you today."
