f9b99b9d922fbbcbec370879a8ad48311df29394
Jose Miguel Alejandro Sepulveda Pino <josealejandrosepulvedapino@gmail.com>
2022-11-29 16:39:31 -0300

    Reto11 terminado

--- app/src/main/java/com/example/kotlinretos/Reto11.kt
+++ app/src/main/java/com/example/kotlinretos/Reto11.kt
@@ -1,5 +1,33 @@
 package com.example.kotlinretos
 
+import kotlin.random.Random
+
 fun main() {
+    var fortune: String
+    for (x in 1..10) {
+        fortune = getFortuneCookie()
+        println("Your fortuine: is $fortune")
+        if (fortune.contains("Take it easy and enjoy life!")) break }
+}
+
+fun getFortuneCookie() :String {
+    val fortunes = listOf(
+        "You will have a great day!n",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.n",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune."
+    )
+    print("Enter your birthday: ")
+    val birthday = readLine()?.toIntOrNull() ?: 1
+    return fortunes[birthday.rem(fortunes.size)]
 
 }
+
+
+
+
+
+
